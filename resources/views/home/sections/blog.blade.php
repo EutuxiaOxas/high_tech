eaa5cffc3298aad9ce71c1a8b2a377c44ff162ff
Alexis Montilla <alexisamm9261@gmail.com>
2021-04-30 11:53:58 -0400

    ordenacion home

---
+++ resources/views/home/sections/blog.blade.php
@@ -0,0 +1,31 @@
+<!-- blog -->
+<section>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-8 text-center">
+        <h2>Artículos de <b>interés.</b></h2>
+      </div>
+    </div>
+    <div class="row">
+          @foreach($posts as $post)
+            <div class="col-md-6 col-lg-4">
+              <article class="card card-minimal">
+                <a href="{{route('main.blog.show', $post->slug)}}" class="card-img-container">
+                  <img class="card-img" src="{{ Storage::url($post->picture) }}" alt="Card image cap">
+                </a>
+                <div class="card-body">
+                  <h5 class="card-title"><a class="text-dark" href="">{{$post->title}}</a></h5>
+                  <span class="card-meta">
+                    @php
+                      $valueaux = substr("$post->content",0,150);
+                      echo $valueaux."[...]";
+                    @endphp
+                   </span>
+                </div>
+              </article>
+            </div>
+          @endforeach
+    </div>
+  </div>
+</section>
+<!-- / blog -->
---
+++ resources/views/home/sections/faq.blade.php
@@ -0,0 +1,80 @@
+<section class="bg-light">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-10 col-lg-6 text-md-center">
+        <h2>¿Quieres <i class="font-weight-bold">Conocernos</i>?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint maiores, consequuntur tempore, odio voluptatem</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="accordion-group accordion-group-minimal" data-accordion-group>
+          <div class="accordion" data-accordion data-aos="fade-up">
+            <div class="accordion-control" data-control>
+              <h5>¿Quienes somos?</h5>
+            </div>
+            <div class="accordion-content" data-content>
+              <div class="accordion-content-wrapper">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quam odit voluptatum, rem libero modi labore porro commodi inventore architecto explicabo reiciendis, perspiciatis voluptatibus odio, sequi nobis? Optio, aperiam, tenetur!</p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion" data-accordion data-aos="fade-up">
+            <div class="accordion-control" data-control>
+              <h5>¿Comó me convierto en DISTRIBUIDOR AUTORIZADO HIGH TECH ?</h5>
+            </div>
+            <div class="accordion-content" data-content>
+              <div class="accordion-content-wrapper">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quam odit voluptatum, rem libero modi labore porro commodi inventore architecto explicabo reiciendis, perspiciatis voluptatibus odio, sequi nobis? Optio, aperiam, tenetur!</p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion" data-accordion data-aos="fade-up">
+            <div class="accordion-control" data-control>
+              <h5>¿Cuales son nuestros puntos de contacto ?</h5>
+            </div>
+            <div class="accordion-content" data-content>
+              <div class="accordion-content-wrapper">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quam odit voluptatum, rem libero modi labore porro commodi inventore architecto explicabo reiciendis, perspiciatis voluptatibus odio, sequi nobis? Optio, aperiam, tenetur!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="accordion-group accordion-group-minimal" data-accordion-group>
+          <div class="accordion" data-accordion data-aos="fade-up">
+            <div class="accordion-control" data-control>
+              <h5>¿Cuantos años en el Mercado ?</h5>
+            </div>
+            <div class="accordion-content" data-content>
+              <div class="accordion-content-wrapper">
+                <p>Una marca con el respaldo de ROLITEC más de 25 años en el mercado de rodamientos técnicos </p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion" data-accordion data-aos="fade-up">
+            <div class="accordion-control" data-control>
+              <h5>¿Como contactarnos?</h5>
+            </div>
+            <div class="accordion-content" data-content>
+              <div class="accordion-content-wrapper">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quam odit voluptatum, rem libero modi labore porro commodi inventore architecto explicabo reiciendis, perspiciatis voluptatibus odio, sequi nobis? Optio, aperiam, tenetur!</p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion" data-accordion data-aos="fade-up">
+            <div class="accordion-control" data-control>
+              <h5>¿Donde descargar catalogo de productos?</h5>
+            </div>
+            <div class="accordion-content" data-content>
+              <div class="accordion-content-wrapper">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quam odit voluptatum, rem libero modi labore porro commodi inventore architecto explicabo reiciendis, perspiciatis voluptatibus odio, sequi nobis? Optio, aperiam, tenetur!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
