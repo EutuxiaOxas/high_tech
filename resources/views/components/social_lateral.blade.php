6971e4dee04c9fa31edf8d22edadc0b36a8572fc
Alexis Montilla <alexisamm9261@gmail.com>
2021-05-24 15:30:53 -0400

    icons rrss

--- resources/views/components/social_lateral.blade.php
+++ resources/views/components/social_lateral.blade.php
@@ -1,12 +1,12 @@
 <div class="lateral_social shadow d-none d-md-block">
     <div class="social_div" style="background-color: #3b5998;">
       <a href="https://www.facebook.com/sharer/sharer.php?u={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/facebook.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('facebook.svg') }}" alt="">
       </a>
     </div>
     <div class="social_div" style="background-color: #0077B5;">
       <a href="http://www.linkedin.com/shareArticle?mini=true&url={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/linkedin.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('linkedin.svg') }}" alt="">
       </a>
     </div>
     <div class="social_div" style="background-color: #FF5700;">
@@ -16,17 +16,17 @@
     </div>
     <div class="social_div" style="background-color: #1DA1F2;">
       <a href="https://twitter.com/intent/tweet?url={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/twitter.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('twitter.svg') }}" alt="">
       </a>
     </div>
     <div class="social_div" style="background-color: #25D366;">
       <a href="https://web.whatsapp.com/send?text={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/whatsapp.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('whatsapp.svg') }}" alt="">
       </a>
     </div>
     <div class="social_div" style="background-color: #0088CC; ">
       <a href="https://telegram.me/share/url?url={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/telegram.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('telegram.svg') }}" alt="">
       </a>
     </div>
 </div>
--- resources/views/components/social_mobile.blade.php
+++ resources/views/components/social_mobile.blade.php
@@ -4,32 +4,32 @@
     </div>
     <div class="col social_div mb-0" style="background-color: #3b5998;">
       <a href="https://www.facebook.com/sharer/sharer.php?u={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/facebook.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('facebook.svg') }}" alt="">
       </a>
     </div>
     <div class="col social_div mb-0" style="background-color: #0077B5;">
       <a href="http://www.linkedin.com/shareArticle?mini=true&url={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/linkedin.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('linkedin.svg') }}" alt="">
       </a>
     </div>
     <div class="col social_div mb-0" style="background-color: #FF5700;">
       <a href="http://reddit.com/submit?url={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/reddit.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('reddit.svg') }}" alt="">
       </a>
     </div>
     <div class="col social_div mb-0" style="background-color: #1DA1F2;">
       <a href="https://twitter.com/intent/tweet?url={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/twitter.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('twitter.svg') }}" alt="">
       </a>
     </div>
     <div class="col social_div mb-0" style="background-color: #25D366;">
       <a href="https://web.whatsapp.com/send?text={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/whatsapp.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('whatsapp.svg') }}" alt="">
       </a>
     </div>
     <div class="col social_div mb-0" style="background-color: #0088CC; ">
       <a href="https://telegram.me/share/url?url={{ Request::url() }}" target="_blank">
-        <img class="social_icon" src="{{ asset('icons/social/telegram.svg') }}" alt="">
+        <img class="social_icon" src="{{ asset('telegram.svg') }}" alt="">
       </a>
     </div>
 </div>
