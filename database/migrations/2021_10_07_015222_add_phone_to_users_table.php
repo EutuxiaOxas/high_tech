1da110a91e45dccb46914f3d1ef2513cf4a5b522
Alexis Montilla <alexisamm9261@gmail.com>
2021-10-06 21:54:15 -0400

    add phone to table users

--- database/migrations/2021_10_07_015222_add_phone_to_users_table.php
+++ database/migrations/2021_10_07_015222_add_phone_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPhoneToUserTable extends Migration
+class AddPhoneToUsersTable extends Migration
 {
     /**
      * Run the migrations.
