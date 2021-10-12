a27b4bba7570327bc71538e0426ce60363eb0ac6
Alexis Montilla <alexisamm9261@gmail.com>
2021-10-12 17:40:55 -0400

    imagenes

--- resources/views/home/sections/banner_principal.blade.php
+++ resources/views/home/sections/banner_principal.blade.php
@@ -31,35 +31,35 @@
                   <div class="row gutter-1">
                       <div class="col text-center">
                         <a href="{{ route('products.category', 'automotriz') }}" aria-label="ver los productos de la categoria automotriz">
-                            <img class="mb-1" src="/imagenes/banner_principal/01.webp" alt="imagen de la categoria automotriz | High Tech Bearings" width="100px" height="100px" loading="lazy">
+                            <img class="mb-1" src="/imagenes/banner_principal/01.webp" alt="imagen de la categoria automotriz | High Tech Bearings" loading="lazy">
                             <div class="text-white font-lg ">SERIE <strong class="font_myriad">AUTOMOTRIZ</strong> </div>
                             <small class="text-white"> <strong class="font_myriad">LINEA NARANJA - AUTOMOTRIZ</strong></small>
                         </a>
                       </div>
                       <div class="col text-center">
                         <a href="{{ route('products.category', 'industrial') }}" aria-label="ver los productos de la categoria industrial">
-                            <img class="mb-1" src="/imagenes/banner_principal/02.webp" alt="imagen de la categoria industrial | High Tech Bearings" width="100px" height="100px" loading="lazy">
+                            <img class="mb-1" src="/imagenes/banner_principal/02.webp" alt="imagen de la categoria industrial | High Tech Bearings" loading="lazy">
                             <div class="text-white font-lg">SERIE <strong class="font_myriad">INDUSTRIAL</strong> </div>
                             <small class="text-white"> <strong class="font_myriad">LINEA VERDE - INDUSTRIAL</strong></small>
                         </a>
                       </div>
                       <div class="col text-center">
                         <a href="{{ route('products.category', 'cadenas') }}" aria-label="ver los productos de la categoria cadenas">
-                            <img class="mb-1" src="/imagenes/banner_principal/03.webp" alt="imagen de la categoria cadenas | High Tech Bearings" width="100px" height="100px" loading="lazy">
+                            <img class="mb-1" src="/imagenes/banner_principal/03.webp" alt="imagen de la categoria cadenas | High Tech Bearings" loading="lazy">
                             <div class="text-white font-lg">SERIE <strong class="font_myriad">CADENAS</strong> </div>
                             <small class="text-white"> <strong class="font_myriad">LINEA AMARILLA - CADENAS</strong></small>
                         </a>
                       </div>
                       <div class="col text-center">
                         <a href="{{ route('products.category', 'chumacera') }}" aria-label="ver los productos de la categoria chumacera">
-                            <img class="mb-1" src="/imagenes/banner_principal/04.webp" alt="imagen de la categoria chumaceras | High Tech Bearings" width="100px" height="100px" loading="lazy">
+                            <img class="mb-1" src="/imagenes/banner_principal/04.webp" alt="imagen de la categoria chumaceras | High Tech Bearings" loading="lazy">
                             <div class="text-white font-lg">SERIE <strong class="font_myriad">CHUMACERA</strong> </div>
                             <small class="text-white"> <strong class="font_myriad">LINEA AZUL - CHUMACERAS</strong> </small>
                         </a>
                       </div>
                       <div class="col text-center">
                         <a href="{{ route('products.category', 'moto') }}" aria-label="ver los productos de la categoria moto">
-                            <img class="mb-1" src="/imagenes/banner_principal/05.webp" alt="imagen de la categoria motos | High Tech Bearings" width="100px" height="100px" loading="lazy">
+                            <img class="mb-1" src="/imagenes/banner_principal/05.webp" alt="imagen de la categoria motos | High Tech Bearings" loading="lazy">
                             <div class="text-white font-lg">SERIE <strong class="font_myriad">MOTO</strong> </div>
                             <small class="text-white"> <strong class="font_myriad">LINEA ROJA - MOTOCICLETAS</strong></small>
                         </a>
