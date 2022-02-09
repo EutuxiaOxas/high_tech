2991c7f6d0a455ac962c3333c361c98aef2472c2
Alexis Montilla <alexisamm9261@gmail.com>
2022-02-09 17:55:12 -0400

    Limpiar texto de entidades html

--- resources/views/home/sections/blog.blade.php
+++ resources/views/home/sections/blog.blade.php
@@ -10,22 +10,22 @@
         @foreach($posts as $post)
         <div class="col-md-6 col-lg-4">
             <article class="card card-minimal">
-            <a href="{{route('main.blog.show', $post->slug)}}" class="card-img-container" aria-label="ver el articulo">
-                <img class="card-img" src="{{ Storage::url($post->picture) }}" alt="{{ ucfirst(strtolower($post->title)) }} | High Tech" loading="lazy" width="100%" height="auto">
-            </a>
-            <div class="card-body">
+              <a href="{{route('main.blog.show', $post->slug)}}" class="card-img-container" aria-label="ver el articulo">
+                  <img class="card-img" src="{{ Storage::url($post->picture) }}" alt="{{ ucfirst(strtolower($post->title)) }} | High Tech" loading="lazy" width="100%" height="auto">
+              </a>
+              <div class="card-body">
                 <h5 class="card-title">
-                <a class="text-dark" href="{{route('main.blog.show', $post->slug)}}" aria-label="ver el articulo">
-                    {{ ucfirst(strtolower($post->title)) }}
-                </a>
-            </h5>
+                    <a class="text-dark" href="{{route('main.blog.show', $post->slug)}}" aria-label="ver el articulo">
+                        {{ ucfirst(strtolower($post->title)) }}
+                    </a>
+                </h5>
                 <span class="card-meta">
-                @php
-                    $valueaux = substr("$post->content",0,150);
-                    echo $valueaux."[...]";
-                @endphp
+                  @php
+                      $valueaux = substr("$post->content",0,150);
+                      echo strip_tags($valueaux)."[...]";
+                  @endphp
                 </span>
-            </div>
+              </div>
             </article>
         </div>
         @endforeach
